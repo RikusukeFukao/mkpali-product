7238e978ba9e0438da2a01084acabf6492e15a58
Shiho.Komuro <shiho.komuro@rakuten.com>
2018-07-06 03:59:15 +0000

    countdown

--- resources/views/mkpali/ng.blade.php
+++ resources/views/mkpali/ng.blade.php
@@ -1,1 +1,15 @@
-tachituteto
+@extends('layouts.app')
+
+@section('content')
+
+
+君の人生の中で一番若いのは今日だよね！？
+僕はそうだと思ってましたけど！！？？？
+
+
+
+
+{!! link_to_route('show1.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+
+
+@endsection
