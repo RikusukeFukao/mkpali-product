b75b4ca12d1f848b7886c73a0c4478e6d441f63f
taro.kirameki <taro.kirameki@techacademy.jp>
2018-07-12 06:39:26 +0000

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to Mkpali</h1>
-                {!! link_to_route('show1.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+<head> <link rel="stylesheet" href="{{ secure_asset('css/welcome.css') }}"></head>
+            <div class="text-center span">
+                <h1>この画面を見ているそこの君！<br>
+                    <br>
+                    退屈な日常にうんざりしているんだろ？<br>
+                    <br>
+                    さあ、今から自分の殻を破りに行こうぜ！<br>
+                    <br>
+                    Are You Ready? Here We Go!!!
+                    <br>
+                </h1>
+                </font>
+                <a href='show1' class="btn">
+	            <i class="fa fa-power-off"></i>
+                </a>
             </div>
-        </div>
 @endsection
