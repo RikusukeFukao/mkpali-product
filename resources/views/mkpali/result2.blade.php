e96427d7055579e557d98222ac40b4976d3fc851
rikusuke.fukao <rikusuke.a.fukao@rakuten.com>
2018-07-18 07:31:17 +0000

    yukahelp2 resultfont

--- resources/views/mkpali/result2.blade.php
+++ resources/views/mkpali/result2.blade.php
@@ -30,7 +30,8 @@
         <!--パチンコスロットの流れ-->
         <div id="wrap">
             <h3>パチンコ・スロットの流れ</h3>
-                <p>①お金を入れる<br>
+                <p>
+                ①お金を入れる<br>
                 ②プレイする<br>
                 大当たりが出るまで①、②の繰り返し。
                 </p>
@@ -78,15 +79,15 @@
             
         <div id="map" style="width:400px; height:300px"></div>
 
-            <script type="text/javascript" charset="utf-8" src="https://map.yahooapis.jp/js/V1/jsapi?..."></script>
-            <script type="text/javascript">
-            window.onload = function(){
-            var ymap = new Y.Map("map");
-            ymap.drawMap(new Y.LatLng(35.600638, 139.611358), 17, Y.LayerSetId.NORMAL);
+        <script type="text/javascript" charset="utf-8" src="https://map.yahooapis.jp/js/V1/jsapi?appid=dj00aiZpPUVUWnNBcEZlcWxPOSZzPWNvbnN1bWVyc2VjcmV0Jng9ZDE-"></script>
+        <script type="text/javascript">
+        window.onload = function(){
+        var ymap = new Y.Map("map");
+        ymap.drawMap(new Y.LatLng(35.600638, 139.611358), 17, Y.LayerSetId.NORMAL);
 };
-            </script>
+        </script>
         </div>
-    <body>
+    </body>
     
 </html>
 @endsection
