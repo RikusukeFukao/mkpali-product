d307bfe76bfab6d7d1327c1982f47512d0356fdf
sayuri.yamaguchi <sayuri.a.yamaguchi@rakuten.com>
2018-07-24 07:15:44 +0000

    gazou_up

--- resources/views/mkpali/result7.blade.php
+++ resources/views/mkpali/result7.blade.php
@@ -62,9 +62,13 @@
             <h3>最強の特殊ヘアを実現！CLIPJOINTGOD！！</h3>
                 <p>
                     できる髪型...<span id="thick">レインボー、スイカ</span>など<br>
-                    
-                   <img src="./images/ahuroo.png" alt="りくぞう">   
-                     <img src="./images/suika copy.png" alt="りくぞう">
+                 
+                 
+            <body>
+                 <div class="niji">   
+                   <img src="./images/ahuroo.png" alt="りくぞう"><img src="./images/suika copy.png" alt="りくぞう">
+                </div>
+           </body> 
                     
                     ＪＲ山手線巣鴨駅徒歩4分<br>
                     都営三田線巣鴨駅徒歩2分<br>
@@ -89,8 +93,11 @@
                 <p>
                     できる髪型...<span id="thick">レオパード柄、あほ毛ハート</span>など<br>
                    
-                     <img src="./images/hyou.png" alt="りくぞう">
-                     <img src="./images/ha-to.png" alt="りくぞう">
+            <div class="kamigata2">     
+                 <div class="hyou">
+                     <img src="./images/hyou.png" alt="りくぞう"><img src="./images/ha-to.png" alt="りくぞう">
+                </div>   
+            </div>
                    
                     下北沢駅徒歩３０秒<br>
                 </p>
