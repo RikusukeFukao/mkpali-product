31b04363bd73959c59bbb4c3ebf1e13ef35f9e73
sayuri.yamaguchi <sayuri.a.yamaguchi@rakuten.com>
2018-07-25 01:18:48 +0000

    rikuzou

--- resources/views/mkpali/result7.blade.php
+++ resources/views/mkpali/result7.blade.php
@@ -8,7 +8,7 @@
 
 <body>
     <div class="rikuzou">
-        <img src="./images/syuuzou.gif" alt="りくぞう">
+        <img src="./images/gattu.png" alt="りくぞう">
     </div>       
           
     <!--最初のコメント -->
